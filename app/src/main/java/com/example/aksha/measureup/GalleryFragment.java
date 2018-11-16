93798ac5bf46b3a81495caed929cb038021ff6e0
makatz2 <36422351+makatz2@users.noreply.github.com>
2018-11-16 16:34:42 -0600

    This stuff works

--- app/src/main/java/com/example/aksha/measureup/GalleryFragment.java
+++ app/src/main/java/com/example/aksha/measureup/GalleryFragment.java
@@ -4,16 +4,53 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class GalleryFragment extends Fragment {
-
+    GridView gridView;
+    public String[] items ={"Hello","I'm","a","cool","dude","!"};
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_gallery, container, false);
+        gridView = (GridView)rootView.findViewById(R.id.gallery_grid_dynamic);
+        gridView.setAdapter(new GridAdapter(getActivity(), items));
+
+        return rootView;
     }
+
+
+
+//    public void itemClicked(int position) {
+//
+//        if(position == 0){
+//            mp = MediaPlayer.create(getActivity(), R.raw.sound1);
+//            mp.start();
+//        }else if(position == 1){
+//            mp = MediaPlayer.create(getActivity(), R.raw.sound2);
+//            mp.start();
+//
+//        }else if(position == 2) {
+//            mp = MediaPlayer.create(getActivity(), R.raw.sound3);
+//            mp.start();
+//        }
+//        else if(position == 3) {
+//            mp = MediaPlayer.create(getActivity(), R.raw.sound4);
+//            mp.start();
+//        }
+//        else if(position == 4) {
+//            mp = MediaPlayer.create(getActivity(), R.raw.sound5);
+//            mp.start();
+//        }
+//        else if(position == 5) {
+//            mp = MediaPlayer.create(getActivity(), R.raw.sound6);
+//            mp.start();
+//        }
+
+//    }
+
 }
