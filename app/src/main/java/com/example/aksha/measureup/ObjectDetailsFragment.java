58a667346e4d75e35ebc4cf995f7c6d694da37a7
SchmidtDavid <schmidt.david125@gmail.com>
2018-12-05 15:13:19 -0600

    Object view works :)

--- app/src/main/java/com/example/aksha/measureup/ObjectDetailsFragment.java
+++ app/src/main/java/com/example/aksha/measureup/ObjectDetailsFragment.java
@@ -1,9 +1,14 @@
 package com.example.aksha.measureup;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.aksha.DataBase.VideoObject;
 
@@ -21,16 +26,29 @@ public class ObjectDetailsFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        final View rootView = inflater.inflate(R.layout.fragment_object_details, container, false);
+
 
         videoObjectViewModel = ViewModelProviders.of(getActivity()).get(VideoObjectViewModel.class);
 
         videoObjectViewModel.getCurrentVideoObject().observe(getActivity(), new Observer<VideoObject>() {
             @Override
             public void onChanged(VideoObject videoObject) {
+                TextView text = rootView.findViewById(R.id.name);
+                ImageView thumbnail = rootView.findViewById(R.id.thumbnail);
+//                ListView measurements = rootView.findViewById(R.id.measurementList);
+
+                text.setText(videoObject.getVideoName());
+                thumbnail.setImageBitmap(BitmapFactory.decodeFile(videoObject.getThumbnailPath()));
 
             }
         });
+        return rootView;
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 }
