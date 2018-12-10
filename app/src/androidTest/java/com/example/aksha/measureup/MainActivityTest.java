ed1115d72d9c4c550bf1961113a443205fbbbe75
ShubhenduJoshi <joshishubhendu03@gmail.com>
2018-12-09 22:11:24 -0600

    Testing Implemented (Except Object database and PIN confirmation)

--- app/src/androidTest/java/com/example/aksha/measureup/MainActivityTest.java
+++ app/src/androidTest/java/com/example/aksha/measureup/MainActivityTest.java
@@ -1,6 +1,8 @@
 package com.example.aksha.measureup;
 
+
 import android.view.View;
+
 
 import org.hamcrest.Matcher;
 import org.junit.Before;
@@ -9,13 +11,18 @@ import org.junit.Test;
 
 import androidx.test.core.app.ActivityScenario;
 
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.IsNot.not;
 
 
 public class MainActivityTest {
@@ -25,37 +32,274 @@ public class MainActivityTest {
         ActivityScenario.launch(MainActivity.class);
     }
 
+    public void goToRecordScreen(){
+
+        // wait for the app to load
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Matcher<View> enterPassword = withId(R.id.editText3);
+        onView(enterPassword).perform(typeText("1234"), closeSoftKeyboard());
+
+        onView(withText("ENTER")).perform(click());
+
+        // wait for feature points to appear
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // Checking the correctness of logIn page after the pin has been setup
+    @Test
+    public void testingLogInPage(){
+
+        // wait for the app to load
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Matcher<View> enterPassword = withId(R.id.editText3);
+        onView(enterPassword).check(matches(isDisplayed()));
+
+        // Checking LogIn buttons
+        onView(withText("ENTER")).check(matches(isDisplayed()));
+        onView(withText("ENTER")).check(matches(isClickable()));
+        onView(withText("FORGOT?")).check(matches(isDisplayed()));
+        onView(withText("FORGOT?")).check(matches(isClickable()));
+
+    }
 
     @Test
-    public void testSettingsDisplay() {
+    public void testingSettingsButton() {
 
-        onView(withId(R.id.imageButton)).check(matches(isClickable()));
-        onView(withId(R.id.imageButton)).check(matches(isDisplayed()));
+        this.goToRecordScreen();
+
         Matcher<View> settings = withId(R.id.imageButton);
-
         onView(settings).perform(click());
-        onView(withText("Password")).check(matches(isDisplayed()));
-        onView(withText("Security Question")).check(matches(isDisplayed()));
 
-        onView(withId(R.id.button2)).check(matches(isClickable()));
-        onView(withId(R.id.button2)).check(matches(isDisplayed()));
-        Matcher<View> password = withId(R.id.button2);
+        // wait for next page to load
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Matcher<View> password = withText("Password");
+        Matcher<View> securityQ = withText("Security Question");
+
+        // Checking the pwd and sQ buttons
+        onView(password).check(matches(isDisplayed()));
+        onView(securityQ).check(matches(isDisplayed()));
+        onView(password).check(matches(isClickable()));
+        onView(securityQ).check(matches(isClickable()));
+
         onView(password).perform(click());
 
+        // wait for next page to load
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        onView(withId(R.id.button10)).check(matches(isClickable()));
-        onView(withId(R.id.button10)).check(matches(isDisplayed()));
-        Matcher<View> securityQuestion = withId(R.id.button2);
-        onView(securityQuestion).perform(click());
+        Matcher<View> disablePassword = withText("Disable Password");
+        Matcher<View> changePassword = withText("Change Password");
+
+        // Checking the password buttons
+        onView(changePassword).check(matches(isDisplayed()));
+        onView(changePassword).check(matches(isClickable()));
+        onView(disablePassword).check(matches(isDisplayed()));
+        onView(disablePassword).check(matches(isClickable()));
+
+        onView(disablePassword).perform(click());
+
+        // wait for next page to load
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Matcher<View> cancel = withText("CANCEL");
+        Matcher<View> ok = withText("OK");
+
+        // Checking the disablePassword dialogue
+        onView(cancel).check(matches(isDisplayed()));
+        onView(cancel).check(matches(isClickable()));
+        onView(ok).check(matches(isDisplayed()));
+        onView(ok).check(matches(isClickable()));
+
+        onView(withText("Verify Password")).check(matches(isDisplayed()));
+
+        onView(cancel).perform(click());
+
+        // wait for next page to load
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(changePassword).perform(click());
+
+        // wait for next page to load
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Matcher<View> confirm = withText("CONFIRM");
+        onView(confirm).check(matches(isDisplayed()));
+        onView(confirm).check(matches(isClickable()));
+
+        onView(withText("Change Password")).check(matches(isDisplayed()));
+        onView(withId(R.id.editText4)).check(matches(isDisplayed()));
+        onView(withId(R.id.editText10)).check(matches(isDisplayed()));
+        onView(withId(R.id.editText11)).check(matches(isDisplayed()));
+
     }
-    @Test
-    public void testGalleryDisplay() {
 
+    // Checking for dialogue box, buttons and the reference distance
+    @Test
+    public void testingReferenceDistance(){
+
+        this.goToRecordScreen();
+
+        // Checking the settings and gallery button
+        onView(withId(R.id.imageButton)).check(matches(isClickable()));
+        onView(withId(R.id.imageButton)).check(matches(isDisplayed()));
         onView(withId(R.id.imageButton2)).check(matches(isClickable()));
         onView(withId(R.id.imageButton2)).check(matches(isDisplayed()));
-        Matcher<View> gallery = withId(R.id.imageButton2);
 
-        onView(gallery).perform(click());
-        onView(withText("Gallery")).check(matches(isDisplayed()));
+        Matcher<View> recordView = withId(R.id.recordButtonView);
+        // click record button (start recording)
+        onView(recordView).perform(click());
+
+        // wait for app to record for 5s
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // stop recording
+        onView(recordView).perform(click());
+
+        // wait for dialogue box to appear
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Checking the Save button
+        Matcher<View> saveView = withText("Save");
+        onView(saveView).check(matches(isClickable()));
+        onView(saveView).check(matches(isDisplayed()));
+
+        // Checking the discard button
+        Matcher<View> discardView = withText("Discard");
+        onView(discardView).check(matches(isClickable()));
+        onView(discardView).check(matches(isDisplayed()));
+
+        onView(discardView).perform(click());
+
+        // Checking whether reference distance is being displayed or not
+        onView(withId(R.id.textView)).check(matches(not(withText(""))));
+
+    }
+
+    // Checking whether the results are being displayed or not (This is right after saving the object by clicking "yes")
+    @Test
+    public void testingAlgorithm() {
+
+        this.goToRecordScreen();
+
+        Matcher<View> recordView = withId(R.id.recordButtonView);
+
+        // click record button (start recording)
+        onView(recordView).perform(click());
+
+        // wait for app to record for 5s
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // stop recording
+        onView(recordView).perform(click());
+
+        // wait for dialogue box to appear
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withHint("Enter a title for the object")).perform(typeText("randomObject"), closeSoftKeyboard());
+
+        // Checking the Save button
+        Matcher<View> saveView = withText("Save");
+        onView(saveView).perform(click());
+
+        // wait for second dialogue box to appear
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withText("Your object has been saved in the gallery!\nWould you like to measure it?")).check(matches(isDisplayed()));
+
+        // Checking the Yes button
+        Matcher<View> yesView = withText("Yes");
+        onView(yesView).check(matches(isClickable()));
+        onView(yesView).check(matches(isDisplayed()));
+
+        // Checking the No button
+        Matcher<View> noView = withText("No");
+        onView(noView).check(matches(isClickable()));
+        onView(noView).check(matches(isDisplayed()));
+
+        onView(yesView).perform(click());
+
+        // wait for pointers to appear
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Checking the measure button
+        Matcher<View> measureView = withText("MEASURE");
+        onView(measureView).check(matches(isClickable()));
+        onView(measureView).check(matches(isDisplayed()));
+
+        onView(measureView).perform(click());
+
+        // waiting for result to appear
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withText("Results")).check(matches(isDisplayed()));
+    }
+
+    // Checking whether the object is getting saved/deleted or not
+    @Test
+    public void testingObjectDatabase() {
+
     }
 }
